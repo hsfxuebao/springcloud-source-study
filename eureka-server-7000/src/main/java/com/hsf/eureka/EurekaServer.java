cd54525054591b0e85a721196a8cfaf3c546dc13
haoshaofei <haoshaofei@kuaishou.com>
2022-10-10 19:59:21 +0800

    eureka-server项目搭建

---
+++ eureka-server-7000/src/main/java/com/hsf/eureka/EurekaServer.java
@@ -0,0 +1,18 @@
+package com.hsf.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author haoshaofei <haoshaofei@kuaishou.com>
+ * Created on 2022-10-10
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+}
